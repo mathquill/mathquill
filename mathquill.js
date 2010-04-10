f2194c47da6ff924c6528b0ecb04792c1b5b20f6
Han Wei <laughinghan@gmail.com>
2010-04-09 23:00:24 -0700

    the css isn't loading in the live demo?

--- mathquill.js
+++ mathquill.js
@@ -10,7 +10,7 @@
 
 jQuery.fn.mathquill = (function($){ //takes in the jQuery function as an argument
 
-$(document.head).append('<link rel="stylesheet" type="text/css" href="http://laughinghan.github.com/mathquill/mathquill.css">');
+$(document.head).prepend('<link rel="stylesheet" type="text/css" href="http://laughinghan.github.com/mathquill/mathquill.css">');
 
 var noop = function(){ return this; }, todo = function(){ alert('BLAM!\n\nAHHHHHH!\n\n"Oh god, oh god, I\'ve never seen so much blood!"\n\nYeah, that doesn\'t fully work yet.'); };
 /**
