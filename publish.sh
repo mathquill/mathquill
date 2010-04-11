c97b8230825385ee51e15060d34272ffaef7e2fd
Han Wei <laughinghan@gmail.com>
2010-04-11 15:35:51 -0700

    now publish.sh automatically uncomments the stylesheet appender

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@ cp mathquill.css build/mathquill.css
 branch=`git branch | grep '\*' | sed 's/\* *//'`
 git checkout gh-pages
 rm -f mathquill.js mathquill.css
-cp build/mathquill.js mathquill.js
+cat build/mathquill.js | sed 's/\/\/\$(/$(/1' > mathquill.js
 cp build/mathquill.css mathquill.css
 rm -f build/mathquill.css
 git commit -a -m "publish new mathquill.js"
