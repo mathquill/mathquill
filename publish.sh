da1415bb7529f0e5375c99f6bfba3fa27c0c81de
Han Wei <laughinghan@gmail.com>
2010-04-10 20:39:28 -0700

    fix publish.sh

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
-branch=master
+branch=`git branch | grep '\*' | sed 's/\* *//'`
 git checkout gh-pages
 rm -f mathquill.js
 cp build/mathquill.js mathquill.js
+git commit -a -m "publish new mathquill.js"
 git push
-git checkout 
+git checkout $branch
