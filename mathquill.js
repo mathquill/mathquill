2c689c6f04b061b0c2324de74c800eb9a3230bcd
Han Wei <laughinghan@gmail.com>
2010-04-11 14:58:12 -0700

    publish new mathquill.js

--- mathquill.js
+++ mathquill.js
@@ -10,7 +10,7 @@
 
 jQuery.fn.mathquill = (function($){ //takes in the jQuery function as an argument
 
-//$('head').append('<link rel="stylesheet" type="text/css" href="http://laughinghan.github.com/mathquill/mathquill.css">');
+$('head').append('<link rel="stylesheet" type="text/css" href="http://laughinghan.github.com/mathquill/mathquill.css">');
 
 var todo = function(){ alert('BLAM!\n\nAHHHHHH!\n\n"Oh god, oh god, I\'ve never seen so much blood!"\n\nYeah, that doesn\'t fully work yet.'); };
 
@@ -379,7 +379,7 @@ function Paren(open, close, replacedFragment)
   this.firstChild.jQ.change(function()
   {
     var block = $(this);
-    block.prev().add(block.next()).css('fontSize', block.height()/(+block.css('fontSize').slice(0,-2)+1)+'em');
+    block.prev().add(block.next()).css('fontSize', block.height()/(+block.css('fontSize').slice(0,-2)+2)+'em');
   });
 }
 Paren.prototype = $.extend(new MathCommand, {
