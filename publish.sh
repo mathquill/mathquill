4804a27b2877d292ac45934eb15d0d7ef5d22cd7
Han Wei <laughinghan@gmail.com>
2010-04-10 23:48:34 -0700

    publish stylesheet too

--- publish.sh
+++ publish.sh
@@ -1,7 +1,10 @@
+cp mathquill.css build/mathquill.css
 branch=`git branch | grep '\*' | sed 's/\* *//'`
 git checkout gh-pages
-rm -f mathquill.js
+rm -f mathquill.js mathquill.css
 cp build/mathquill.js mathquill.js
+cp build/mathquill.css mathquill.css
+rm -f build/mathquill.css
 git commit -a -m "publish new mathquill.js"
 git push
 git checkout $branch
--- src/intro.js
+++ src/intro.js
@@ -10,7 +10,7 @@
 
 jQuery.fn.mathquill = (function($){ //takes in the jQuery function as an argument
 
-$('head').append('<link rel="stylesheet" type="text/css" href="http://laughinghan.github.com/mathquill/mathquill.css">');
+//$('head').append('<link rel="stylesheet" type="text/css" href="http://laughinghan.github.com/mathquill/mathquill.css">');
 
 var todo = function(){ alert('BLAM!\n\nAHHHHHH!\n\n"Oh god, oh god, I\'ve never seen so much blood!"\n\nYeah, that doesn\'t fully work yet.'); };
 
