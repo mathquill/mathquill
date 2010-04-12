9b5a195996bf1de46729ba61c622c1755bada392
Han Wei <laughinghan@gmail.com>
2010-04-11 20:08:14 -0700

    publish new mathquill.js

--- mathquill.js
+++ mathquill.js
@@ -1,17 +1,18 @@
 /**
 * Usage:
 * $(thing).mathquill();
-* turns thing into a live editable thingy.
+* turns thing into a live editable math thingy.
 * AMAZORZ.
 *
-* Note: doesn't actually work.
+* Note: turning into a live editable math thingmajiggie works, but
+* any LaTeX math in it won't be rendered.
 *
 */
 
 jQuery.fn.mathquill = (function($){ //takes in the jQuery function as an argument
 
-//Note: the following must be on line 14 for publish.sh to work
-$('head').append('<link rel="stylesheet" type="text/css" href="http://laughinghan.github.com/mathquill/mathquill.css">');
+Note: the following must be on line 14 for publish.sh to work
+//$('head').append('<link rel="stylesheet" type="text/css" href="http://laughinghan.github.com/mathquill/mathquill.css">');
 
 var todo = function(){ alert('BLAM!\n\nAHHHHHH!\n\n"Oh god, oh god, I\'ve never seen so much blood!"\n\nYeah, that doesn\'t fully work yet.'); };
 
