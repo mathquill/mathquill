502ffe517e5b33de48431027226a056cb0537499
Han Wei <laughinghan@gmail.com>
2010-04-11 19:58:53 -0700

    publish new mathquill.js

--- publish.sh
+++ publish.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+
 # According to Joel Spolsky, it's important to have a single-
 # step build process because when the deadline is looming and
 # we're rushing to fix the "last" bug, an involved, multi-
 # step build process is begging for mistakes
+
 cp mathquill.css build/mathquill.css
 branch=`git branch | grep '\*' | sed 's/\* *//'`
 git checkout gh-pages
 rm -f mathquill.js mathquill.css
-cat build/mathquill.js | sed 's/\/\/\$(/$(/1' > mathquill.js
+cat build/mathquill.js | sed '14s://::' > mathquill.js
 cp build/mathquill.css mathquill.css
 rm -f build/mathquill.css
 git commit -a -m "publish new mathquill.js"
 git push
 git checkout $branch
+echo 'lol!'
