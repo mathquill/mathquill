3c0277e6d6656eafd7427e3dc54cec36831e3160
Han Wei <laughinghan@gmail.com>
2010-04-11 20:20:10 -0700

    publish new mathquill.js

--- mathquill.js
+++ mathquill.js
@@ -11,8 +11,8 @@
 
 jQuery.fn.mathquill = (function($){ //takes in the jQuery function as an argument
 
-Note: the following must be on line 14 for publish.sh to work
-//$('head').append('<link rel="stylesheet" type="text/css" href="http://laughinghan.github.com/mathquill/mathquill.css">');
+//Note: if the following is no longer on line 15, please modify publish.sh accordingly
+$('head').append('<link rel="stylesheet" type="text/css" href="http://laughinghan.github.com/mathquill/mathquill.css">');
 
 var todo = function(){ alert('BLAM!\n\nAHHHHHH!\n\n"Oh god, oh god, I\'ve never seen so much blood!"\n\nYeah, that doesn\'t fully work yet.'); };
 
