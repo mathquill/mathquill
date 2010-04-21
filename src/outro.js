6388c791da12a800b8e6f2eaf433a5c23208695d
Han Wei <laughinghan@gmail.com>
2010-04-21 12:24:49 -0700

    publish new mathquill.js

--- src/outro.js
+++ src/outro.js
@@ -3,6 +3,8 @@
 $(function(){
   $('.mathquill-embedded-latex').mathquill();
   $('.mathquill-editable').mathquill('editable');
+  var _onerror = window.onerror;
+  window.onerror = function(){ if(_onerror) _onerror.apply(this, arguments); return true; };
 });
 
 return mathquill;
