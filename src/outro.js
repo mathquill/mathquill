40b4a999902e334485a6aa05ab75350cab07b9f1
Han Wei <laughinghan@gmail.com>
2010-04-21 14:32:35 -0700

    error suppressing doesn't help at all in IE

--- src/outro.js
+++ src/outro.js
@@ -3,12 +3,6 @@
 $(function(){
   $('.mathquill-embedded-latex').mathquill();
   $('.mathquill-editable').mathquill('editable');
-  var _onerror = window.onerror;
-  window.onerror = function(){
-    if(_onerror)
-      _onerror.apply(this, arguments);
-    return true;
-  };
 });
 
 return mathquill;
