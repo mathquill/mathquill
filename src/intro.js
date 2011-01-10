78961c026b4870e60281c3a3a2d657afabbd6c9f
Han Wei <laughinghan@gmail.com>
2011-01-10 13:03:45 -0800

    better `todo()`

--- src/intro.js
+++ src/intro.js
@@ -7,5 +7,5 @@
 //Note: if the following is no longer on line 12 of build/mathquill.js, please modify publish.sh accordingly
 //$('head').prepend('<link rel="stylesheet" type="text/css" href="http://laughinghan.github.com/mathquill/mathquill.css">');
 
-function todo(){ alert('BLAM!\n\nAHHHHHH!\n\n"Oh god, oh god, I\'ve never seen so much blood!"\n\nYeah, that doesn\'t fully work yet.'); };
+function todo(){ alert('BLAM!\n\nAHHHHHH!\n\n"Oh god, oh god, I\'ve never seen so much blood!"\n\nYeah, '+todo.caller.name+' doesn\'t fully work yet.'); }
 
