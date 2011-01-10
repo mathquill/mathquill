d7581a4b9c173876db40daaaef8673ccefb32ab6
Han Wei <laughinghan@gmail.com>
2011-01-10 05:48:43 -0800

    fuck "local changes...cannot switch branches."

--- publish.sh
+++ publish.sh
@@ -5,6 +5,7 @@
 # we're rushing to fix the "last" bug, an involved, multi-
 # step build process is begging for mistakes
 
+git stash
 cp mathquill.css build/mathquill.css
 branch=`git branch | grep '\*' | sed 's/\* *//'`
 git checkout gh-pages
@@ -15,3 +16,4 @@ rm build/mathquill.css
 git commit -a -m "publish new mathquill.js"
 git push
 git checkout $branch
+git stash pop
