14a1e904a4b28125e097698eb22f685327a6cfdb
Han Wei <laughinghan@gmail.com>
2011-01-11 12:37:42 -0800

    <link rel=stylesheet> now required, don't prepend

--- src/intro.js
+++ src/intro.js
@@ -4,6 +4,3 @@
 
 (function($){ //takes in the jQuery function as an argument
 
-//Note: if the following is no longer on line 12 of build/mathquill.js, please modify publish.sh accordingly
-//$('head').prepend('<link rel="stylesheet" type="text/css" href="http://laughinghan.github.com/mathquill/mathquill.css">');
-
