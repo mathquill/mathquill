1b5c25bb896047a7c8caa8a4dc688da20608cc71
alvin <alvin.savoy@gmail.com>
2011-02-16 17:47:19 +1100

    Added browser-sniffing for IE

--- src/intro.js
+++ src/intro.js
@@ -6,4 +6,6 @@
 
 var _, //temp variable of prototypes
   undefined,
+  isIE7 = $.browser.msie && (parseInt($.browser.version) == 7), // Browser sniffing
+  isIE8 = $.browser.msie && (parseInt($.browser.version) == 8),
   jQueryDataKey = '[[mathquill internal data]]';
