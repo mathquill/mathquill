cbe627f35b2a39962d177db0d085e98d51b00109
Han Wei <laughinghan@gmail.com>
2011-05-29 23:17:20 -0400

    publish new dev/mathquill.{js, css}

--- dev/mathquill.js
+++ dev/mathquill.js
@@ -304,10 +304,9 @@ function createRoot(jQ, root, textbox, editable) {
     if (!cursor.parent)
       cursor.appendTo(root);
     cursor.parent.jQ.addClass('hasCursor');
-    if (cursor.selection) {
+    if (cursor.selection)
       cursor.selection.jQ.removeClass('blur');
-      setTimeout(function(){ cursor.selectLatex(); });
-    } else
+    else
       cursor.show();
     e.stopPropagation();
   }).blur(function(e) {
@@ -322,12 +321,7 @@ function createRoot(jQ, root, textbox, editable) {
     var text = textarea.val();
     if (!text) return;
     textarea.val('');
-    // textarea can contain more than one character
-    // when typing quickly on slower platforms;
-    // so process each character separately
-    for (var i=0; i<text.length; i++) {
-        cursor.parent.textInput(text[i]);
-    }
+    cursor.parent.textInput(text);
   }
 
   var lastKeydn = {}; //see Wiki page "Keyboard Events"
@@ -375,18 +369,7 @@ function createRoot(jQ, root, textbox, editable) {
     $(document).mousemove(docmousemove).mouseup(mouseup);
 
     setTimeout(function(){textarea.focus();});
-  }).bind('cut', function() {
-    if (cursor.selection)
-      cursor.deleteSelection();
-  }).bind('paste', function() {
-    setTimeout(function() {
-      cursor.writeLatex(textarea.val()).clearSelection();
-    });
-  }).bind('selectstart.mathquill', function(e) {
-    if (e.target != textarea[0])
-      e.preventDefault();
-    e.stopPropagation();
-  }).blur();
+  }).bind('selectstart.mathquill', false).blur();
 
   function mousemove(e) {
     cursor.seek($(e.target), e.pageX, e.pageY);
@@ -470,11 +453,11 @@ _.keydown = function(e)
     }
 
     this.cursor.clearSelection();
-    break;
+    return false;
   case 13: //enter
   case 'Enter':
     e.preventDefault();
-    return true;
+    break;
   case 35: //end
   case 'End':
     if (e.shiftKey)
@@ -565,16 +548,58 @@ _.keydown = function(e)
       while (this.cursor.prev)
         this.cursor.selectLeft();
       e.preventDefault();
-      return false;
     }
     else
       this.skipTextInput = false;
-    return true;
+    break;
+  case 67: //the 'C' key, as in Ctrl+C Copy
+  case 'C':
+  case 'U+0043':
+    if (e.ctrlKey && !e.shiftKey && !e.altKey) {
+      if (this !== this.cursor.root) //so not stopPropagation'd at RootMathCommand
+        return this.parent.keydown(e);
+
+      if (!this.cursor.selection) return true;
+
+      window['MathQuill LaTeX Clipboard'] = this.cursor.selection.latex();
+      e.preventDefault();
+    }
+    else
+      this.skipTextInput = false;
+    break;
+  case 86: //the 'V' key, as in Ctrl+V Paste
+  case 'V':
+  case 'U+0056':
+    if (e.ctrlKey && !e.shiftKey && !e.altKey) {
+      if (this !== this.cursor.root) //so not stopPropagation'd at RootMathCommand
+        return this.parent.keydown(e);
+
+      this.cursor.writeLatex(window['MathQuill LaTeX Clipboard']).show();
+      e.preventDefault();
+    }
+    else
+      this.skipTextInput = false;
+    break;
+  case 88: //the 'X' key, as in Ctrl+X Cut
+  case 'X':
+  case 'U+0058':
+    if (e.ctrlKey && !e.shiftKey && !e.altKey) {
+      if (this !== this.cursor.root) //so not stopPropagation'd at RootMathCommand
+        return this.parent.keydown(e);
+
+      if (!this.cursor.selection) return true;
+
+      window['MathQuill LaTeX Clipboard'] = this.cursor.selection.latex();
+      this.cursor.deleteSelection();
+      e.preventDefault();
+    }
+    else
+      this.skipTextInput = false;
+    break;
   default:
     this.skipTextInput = false;
-    return true;
   }
-  return false;
+  return true;
 };
 _.textInput = function(ch) {
   if (!this.skipTextInput)
@@ -611,9 +636,6 @@ _.initBlocks = function() {
 
   this.firstChild.parent = this;
   this.firstChild.jQ = this.jQ;
-};
-_.latex = function() {
-  return '$' + this.firstChild.latex() + '$';
 };
 
 function RootTextBlock(){}
@@ -1165,7 +1187,7 @@ _.latex = function() {
   }).join('\\\\') + '\\end{matrix}';
 };
 _.text = function() {
-  return '[' + this.foldChildren([], function(text, child) {
+  return '[' + this.foldChildren([], function(latex, child) {
     text.push(child.text());
     return text;
   }).join() + ']';
@@ -2148,7 +2170,7 @@ _.unwrapGramp = function() {
 
     uncle.eachChild(function(cousin) {
       cousin.parent = greatgramp;
-      cousin.jQ.insertBefore(gramp.jQ.first());
+      cousin.jQ.insertBefore(gramp.jQ);
     });
     uncle.firstChild.prev = prev;
     if (prev)
@@ -2282,7 +2304,6 @@ _.selectFrom = function(anticursor) {
     right.next
   );
   this.insertAfter(right.next.prev || right.parent.lastChild);
-  this.selectLatex();
 };
 _.selectLeft = function() {
   if (this.selection) {
@@ -2310,7 +2331,6 @@ _.selectLeft = function() {
 
     this.hide().selection = new Selection(this.parent, this.prev, this.next.next);
   }
-  this.selectLatex();
 };
 _.selectRight = function() {
   if (this.selection) {
@@ -2338,26 +2358,8 @@ _.selectRight = function() {
 
     this.hide().selection = new Selection(this.parent, this.prev.prev, this.next);
   }
-  this.selectLatex();
-};
-_.selectLatex = function() {
-  var textarea = this.root.textarea.children();
-  var latex = this.selection ? this.selection.latex() : '';
-  textarea.val(latex);
-  if (typeof textarea[0].selectionStart == 'number') {
-    textarea[0].selectionStart = 0;
-    textarea[0].selectionEnd = latex.length;
-  }
-  else if (document.selection) {
-    var range = textarea[0].createTextRange();
-    range.collapse(true);
-    range.moveStart("character", 0);
-    range.moveEnd("character", latex.length);
-    range.select();
-  }
 };
 _.clearSelection = function() {
-  this.root.textarea.children().val('');
   if (this.show().selection) {
     this.selection.clear();
     delete this.selection;
