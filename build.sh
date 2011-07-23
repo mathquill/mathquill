572a0205e6ecd9e5c78038658da9ec480ec3f06a
Han Wei <laughinghan@gmail.com>
2011-07-23 13:45:23 -0400

    fancy new build script, skeleton Makefile

---
+++ build.sh
@@ -0,0 +1,62 @@
+#! /bin/sh
+
+if [ ! -d build ]; then
+  mkdir build \
+    && echo "No build/ directory, made one for ya" >/dev/stderr
+fi
+
+echo "cat src/{blah,blah}.js > build/mathquill.js # not literally"
+cat \
+  src/intro.js \
+  src/baseclasses.js \
+  src/rootelements.js \
+  src/commands.js \
+  src/symbols.js \
+  src/publicapi.js \
+  src/outro.js \
+  > build/mathquill.js
+
+if which uglifyjs >/dev/null; then
+  echo "uglifyjs build/mathquill.js > build/mathquill.min.js"
+  uglifyjs build/mathquill.js \
+    > build/mathquill.min.js
+else
+  # created with cowsay
+  echo "\
+ ___________________
+/ i no has uglifyjs \\
+\\ i no can minify   /
+ -------------------
+  \\
+   \\
+      /\\_)o<
+     |      \\
+     | O . O |
+      \\_____/" \
+    >/dev/stderr
+fi
+
+if [ "$1" == "publish" ]; then
+  cp mathquill.css build/mathquill.css
+  STASH="$(git stash)"
+  echo "$STASH"
+  BRANCH="$(git status | head -1)"
+  COMMIT="$(git show -s --oneline)"
+  git checkout gh-pages
+  echo "git pull origin gh-pages"
+  git pull origin gh-pages
+  cp build/* .
+  rm build/mathquill.css
+  if [ "$BRANCH" != "# Not currently on any branch" ]; then
+    BRANCH="$(echo "$BRANCH" | cut -d ' ' -f 4)"
+    git commit -am "publish latest $BRANCH $COMMIT"
+  else
+    git commit -am "publish detached $COMMIT"
+  fi
+  echo "git push origin gh-pages"
+  git push origin gh-pages
+  git checkout -
+  if [ "$STASH" != "No local changes to save" ]; then
+    git stash pop
+  fi
+fi
