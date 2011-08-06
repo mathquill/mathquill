04e24b71e089a9171b6e2413b6bddf0b1155c895
Han Wei <laughinghan@gmail.com>
2011-08-06 13:58:47 -0400

    standard notation in comments

--- src/symbols.js
+++ src/symbols.js
@@ -303,7 +303,7 @@ LatexCmds.H = LatexCmds.Hamiltonian = LatexCmds.quaternions = LatexCmds.Quaterni
 //spacing
 LatexCmds.quad = LatexCmds.emsp = bind(VanillaSymbol,'\\quad ','    ');
 LatexCmds.qquad = bind(VanillaSymbol,'\\qquad ','        ');
-/* spacing special characters, gonna have to implement this in LatexCommandInput.prototype.textInput somehow
+/* spacing special characters, gonna have to implement this in LatexCommandInput::textInput somehow
 case ',':
   return new VanillaSymbol('\\, ',' ');
 case ':':
