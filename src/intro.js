5792e43bc1eec278274e4a181d9133c85bbda432
Jay Adkisson <jjmadkisson@gmail.com>
2012-06-13 13:59:53 -0700

    the old sugar methods are dead

--- src/intro.js
+++ src/intro.js
@@ -17,47 +17,6 @@ var $ = jQuery,
 var __slice = [].slice;
 
 /**
- * simple sugar for idiomatic JS classes
- * Usage:
- *  var Cat = _class(new Animal, function(furriness) {
- *    this.furriness = furriness;
- *    this.adorableness = furriness/10;
- *  });
- *  _.play = function(){ this.chase('mouse'); };
- */
-function _class(prototype, constructor) {
-  if (!constructor) constructor = function(){};
-  _ = constructor.prototype = prototype;
-  return constructor;
-}
-
-/**
- * more sugar, for classes without a pre-supplied prototype
- * Usage:
- *  var Animal = _baseclass();
- *  _.chase = function(prey) ...
- */
-function _baseclass(constructor) {
-  return _class({}, constructor);
-}
-
-/**
- * sugar specifically for copying the constructor
- * Usage:
- *  var HouseCat = _subclass(Cat);
- *  _.play = function(){ this.chase('yarn'); };
- */
-function _subclass(superclass) {
-  return _class(new superclass, function(){
-    if (superclass.prototype.init) {
-      superclass.prototype.init.apply(this, arguments);
-    } else {
-      superclass.apply(this, arguments);
-    }
-  });
-}
-
-/**
  * sugar to make defining lots of commands easier.
  * TODO: rethink this.
  */
