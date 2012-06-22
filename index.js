0fce6b9f57f1dd533aa969781976ac41d5781801
Jay Adkisson <jjmadkisson@gmail.com>
2012-06-21 18:04:21 -0700

    allow specifying the host to bind to in `make server`

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var connect = require('connect');
 
-var app = connect().use(connect.static(__dirname));
+var app = connect()
+  .use(connect.logger('dev'))
+  .use(connect.static(__dirname))
+;
 
 var terminal = require('child_process').spawn('make', ['dev', 'test']);
 
@@ -8,8 +11,9 @@ terminal.stdout.pipe(process.stdout);
 terminal.stderr.pipe(process.stderr);
 
 var port = +(process.env.PORT || 9292)
+var host = process.env.HOST || 'localhost';
 
 terminal.on('exit', function() {
-  console.log('listening on localhost:'+port)
-  app.listen(port);
+  console.log('listening on '+host+':'+port);
+  app.listen(port, host);
 });
