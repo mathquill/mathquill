7123698fd12504e82ea1906eced49e7923e9880f
Han Wei <laughinghan@gmail.com>
2012-07-05 12:01:02 -0700

    use new template format in SquareRoot

--- src/commands.js
+++ src/commands.js
@@ -211,10 +211,12 @@ var SquareRoot =
 LatexCmds.sqrt =
 LatexCmds['√'] = P(MathCmd, function(_) {
   _.ctrlSeq = '\\sqrt';
-  _.htmlTemplate = [
-    '<span class="sqrt"><span class="non-leaf sqrt-prefix">&radic;</span></span>',
-    '<span class="sqrt-stem"></span>'
-  ];
+  _.htmlTemplate =
+      '<span class="sqrt" #mqCmdId>'
+    +   '<span class="non-leaf sqrt-prefix">&radic;</span>'
+    +   '<span class="sqrt-stem" #mqBlockId:0>#mqBlock:0</span>'
+    + '</span>'
+  ;
   _.textTemplate = ['sqrt(', ')'];
   _.redraw = function() {
     var block = this.lastChild.jQ;
