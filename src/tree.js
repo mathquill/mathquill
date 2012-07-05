449212864a9e49c895270b477417272c116624ac
Jay Adkisson <jjmadkisson@gmail.com>
2012-07-05 12:41:32 -0700

    move ::adopt() to Fragment.

--- src/tree.js
+++ src/tree.js
@@ -18,43 +18,8 @@ var Node = P(function(_) {
   };
 
   _.adopt = function(parent, prev, next) {
-    pray('a parent is always passed to Node::adopt', parent);
-
-    pray('prev is properly set up', (function() {
-      // either it's empty and next is the first child (possibly empty)
-      if (!prev) return parent.firstChild === next;
-
-      // or it's there and its next and parent are properly set up
-      return prev.next === next && prev.parent === parent;
-    })());
-
-    pray('next is properly set up', (function() {
-      // either it's empty and prev is the last child (possibly empty)
-      if (!next) return parent.lastChild === prev;
-
-      // or it's there and its next and parent are properly set up
-      return next.prev === prev && next.parent === parent;
-    })());
-
-    var self = this;
-
-    self.parent = parent;
-    self.next = next;
-    self.prev = prev;
-
-    if (prev) {
-      prev.next = self;
-    } else {
-      parent.firstChild = self;
-    }
-
-    if (next) {
-      next.prev = self;
-    } else {
-      parent.lastChild = self;
-    }
-
-    return self;
+    Fragment(this, this).adopt(parent, prev, next);
+    return this;
   };
 
   _.disown = function() {
@@ -83,8 +48,60 @@ var Fragment = P(function(_) {
   _.last = 0;
 
   _.init = function(first, last) {
+    pray('first node is passed to Fragment', first instanceof Node);
+    pray('last node is passed to Fragment', last instanceof Node);
+
     this.first = first;
     this.last = last;
+  };
+
+  _.adopt = function(parent, prev, next) {
+    pray('a parent is always passed to Fragment::adopt', parent);
+
+    pray('prev is properly set up', (function() {
+      // either it's empty and next is the first child (possibly empty)
+      if (!prev) return parent.firstChild === next;
+
+      // or it's there and its next and parent are properly set up
+      return prev.next === next && prev.parent === parent;
+    })());
+
+    pray('next is properly set up', (function() {
+      // either it's empty and prev is the last child (possibly empty)
+      if (!next) return parent.lastChild === prev;
+
+      // or it's there and its next and parent are properly set up
+      return next.prev === prev && next.parent === parent;
+    })());
+
+    var self = this;
+    var first = self.first;
+    var last = self.last;
+
+    if (prev) {
+      // NB: this is handled in the ::each() block
+      // prev.next = first
+    } else {
+      parent.firstChild = first;
+    }
+
+    if (next) {
+      next.prev = last;
+    } else {
+      parent.lastChild = last;
+    }
+
+    self.last.next = next;
+
+    self.each(function(el) {
+      el.prev = prev;
+      el.parent = parent;
+      if (prev) prev.next = el;
+
+      prev = el;
+    });
+
+    return self;
   };
 
   _.each = function(fn) {
