722acad44121e7045ffc0256bd2a2b239be14dca
Han Wei <laughinghan@gmail.com>
2012-07-05 12:14:04 -0700

    split up Bracket template across lines for readability

--- src/commands.js
+++ src/commands.js
@@ -244,7 +244,11 @@ LatexCmds.nthroot = P(SquareRoot, function(_, _super) {
 var Bracket = P(MathCmd, function(_, _super) {
   _.init = function(open, close, ctrlSeq, end) {
     _super.init.call(this, '\\left'+ctrlSeq,
-      '<span class="non-leaf" #mqCmdId><span class="non-leaf paren">'+open+'</span><span class="non-leaf" #mqBlockId:0>#mqBlock:0</span><span class="non-leaf paren">'+close+'</span></span>',
+        '<span class="non-leaf" #mqCmdId>'
+      +   '<span class="non-leaf paren">'+open+'</span>'
+      +   '<span class="non-leaf" #mqBlockId:0>#mqBlock:0</span>'
+      +   '<span class="non-leaf paren">'+close+'</span>'
+      + '</span>',
       [open, close]);
     this.end = '\\right'+end;
   };
