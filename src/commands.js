8e62d425075715ac85f998a854258ebee4fedcc6
Han Wei <laughinghan@gmail.com>
2012-07-09 19:58:09 -0700

    SupSub set .up and .down props
    way more annoying: ignoring the glaring if/else-instead-of-inheritance
    anti-pattern, which we'll fix when we figure out an decent architecture
    for instantiation of commands with makers instead of bind() and
    constructor args (or something),
    - we set .up or .down on the SupSub to point to the block, depending on
      whether it's a Sup or a Sub,
    - and then we give the block a method that says,
        + on up out of a Sub or down out of a Sup, put the cursor to the
          left of the SupSub (since this is probably the exponent of
          subscript of the thing to the left),
            * unless the cursor is at the end of the SupSub, or at the end
              of something that's at the end of the SupSub, or in general
              at the end of something all of whose ancestors up until the
              SupSub are at the end of their parent blocks
    
    Test cases illustrating why the hell that's desired behavior forthcoming

--- src/commands.js
+++ src/commands.js
@@ -83,7 +83,33 @@ var SupSub = P(MathCommand, function(_, _super) {
   _.init = function(cmd, html, text) {
     _super.init.call(this, cmd, [ html ], [ text ]);
   };
+  _.createBlocks = function() {
+    _super.createBlocks.call(this);
 
+    //TODO: use inheritance
+    if (this.cmd === '_') {
+      this.down = this.firstChild;
+      this.firstChild.up = insertBeforeUnlessAtEnd;
+    }
+    else { pray('SupSub is only _ and ^', this.cmd === '^');
+      this.up = this.firstChild;
+      this.firstChild.down = insertBeforeUnlessAtEnd;
+    }
+    function insertBeforeUnlessAtEnd(cursor) {
+      // cursor.insertBefore(cmd), unless cursor at the end of block, and every
+      // ancestor cmd is at the end of every ancestor block
+      var cmd = this.parent, ancestorCmd = cursor;
+      do {
+        if (ancestorCmd.next) {
+          cursor.insertBefore(cmd);
+          return false;
+        }
+        ancestorCmd = ancestorCmd.parent.parent;
+      } while (ancestorCmd !== cmd);
+      cursor.insertAfter(cmd);
+      return false;
+    }
+  };
   _.latex = function() {
     var latex = this.firstChild.latex();
     if (latex.length === 1)
