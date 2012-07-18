fcfcf27b6aa4b4210d491abb03010a4ad92b31cb
Jay Adkisson <jjmadkisson@gmail.com>
2012-07-18 10:09:59 -0700

    run make when you load the page, not on server start #build

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 var connect = require('connect');
 
+function runMakeMiddleware(req, res, next) {
+  // don't run make when fetching static assets
+  if (/^\/test\/support/.test(req.url)) return next();
+  if (/^\/build/.test(req.url)) return next();
+
+  var terminal = require('child_process').spawn('make', ['dev', 'test']);
+
+  terminal.stdout.pipe(process.stdout);
+  terminal.stderr.pipe(process.stderr);
+
+  terminal.on('exit', next);
+}
+
 var app = connect()
   .use(connect.logger('dev'))
+  .use(runMakeMiddleware)
   .use(connect.static(__dirname))
 ;
-
-var terminal = require('child_process').spawn('make', ['dev', 'test']);
-
-terminal.stdout.pipe(process.stdout);
-terminal.stderr.pipe(process.stderr);
 
 var port = +(process.env.PORT || 9292)
 var host = process.env.HOST || '0.0.0.0';
 
-terminal.on('exit', function() {
-  console.log('listening on '+host+':'+port);
-  app.listen(port, host);
-});
+console.log('listening on '+host+':'+port);
+app.listen(port, host);
