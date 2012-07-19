595be7a954523cb3989367354e1947a023bbc229
Jay Adkisson <jjmadkisson@gmail.com>
2012-07-19 11:17:33 -0700

    fix \editable - overriding jQize doesn't work anymore
    We now override .jQadd, and we don't have to worry about the outer
    cursor writing latex and being weird anymore.

--- src/commands.js
+++ src/commands.js
@@ -766,22 +766,34 @@ LatexCmds.vector = P(MathCommand, function(_, _super) {
 LatexCmds.editable = P(RootMathCommand, function(_, _super) {
   _.init = function() {
     MathCommand.prototype.init.call(this, '\\editable');
-    var cursor;
-    this.createBefore = function(c){ _super.createBefore.call(this, cursor = c); };
-    this.jQize = function() {
-      _super.jQize.call(this);
-      createRoot(this.jQ, this.firstChild, false, true);
-
-      this.firstChild.blur = function() {
-        // when cursor is inserted after editable, append own
-        // cursor FIXME HACK
-        if (cursor.prev !== this.parent) return;
-        delete this.blur;
-        this.cursor.appendTo(this);
-        MathBlock.prototype.blur.call(this);
-      };
-      this.latex = function(){ return this.firstChild.latex(); };
-      this.text = function(){ return this.firstChild.text(); };
-    };
   };
+
+  _.jQadd = function() {
+    var self = this;
+    // FIXME: this entire method is a giant hack to get around
+    // having to call createBlocks, and createRoot expecting to
+    // render the contents' LaTeX. Both need to be refactored.
+    _super.jQadd.apply(self, arguments);
+    var block = self.firstChild.disown();
+    var blockjQ = self.jQ.children().detach();
+
+    self.firstChild =
+    self.lastChild =
+      RootMathBlock();
+
+    self.blocks = [ self.firstChild ];
+
+    self.firstChild.parent = self;
+
+    createRoot(self.jQ, self.firstChild, false, true);
+    self.cursor = self.firstChild.cursor;
+
+    block.children().adopt(self.firstChild, 0, 0);
+    blockjQ.appendTo(self.firstChild.jQ);
+
+    self.firstChild.cursor.appendTo(self.firstChild);
+  };
+
+  _.latex = function(){ return this.firstChild.latex(); };
+  _.text = function(){ return this.firstChild.text(); };
 });
