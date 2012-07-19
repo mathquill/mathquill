fde721004f7193b673808b2928b3f6ffb9297cf5
Jay Adkisson <jjmadkisson@gmail.com>
2012-07-19 10:10:57 -0700

    Revert "run make when you load the page, not on server start #build"
    This reverts commit a3e32d7f6437d6607ce9f571ab0589ece546f1a2.

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 var connect = require('connect');
 
-function runMakeMiddleware(req, res, next) {
-  // don't run make when fetching static assets
-  if (/^\/test\/support/.test(req.url)) return next();
-  if (/^\/build/.test(req.url)) return next();
-
-  var terminal = require('child_process').spawn('make', ['dev', 'test']);
-
-  terminal.stdout.pipe(process.stdout);
-  terminal.stderr.pipe(process.stderr);
-
-  terminal.on('exit', next);
-}
-
 var app = connect()
   .use(connect.logger('dev'))
-  .use(runMakeMiddleware)
   .use(connect.static(__dirname))
 ;
+
+var terminal = require('child_process').spawn('make', ['dev', 'test']);
+
+terminal.stdout.pipe(process.stdout);
+terminal.stderr.pipe(process.stderr);
 
 var port = +(process.env.PORT || 9292)
 var host = process.env.HOST || '0.0.0.0';
 
-console.log('listening on '+host+':'+port);
-app.listen(port, host);
+terminal.on('exit', function() {
+  console.log('listening on '+host+':'+port);
+  app.listen(port, host);
+});
