7491d9130073b14cd2a61d69771002f91ffdd300
Jay Adkisson <jjmadkisson@gmail.com>
2012-07-19 11:11:28 -0700

    fix Binom::redraw, which we obviously didn't test #herpderp

--- src/commands.js
+++ src/commands.js
@@ -641,12 +641,12 @@ LatexCmds.binomial = P(MathCommand, function(_, _super) {
   ;
   _.textTemplate = ['choose(',',',')'];
   _.redraw = function() {
-    var blockjQ = this.jQ.children(':eq(1)');
+    var blockjQ = this.jQ.eq(1);
 
     var height = blockjQ.outerHeight()/+blockjQ.css('fontSize').slice(0,-2);
 
-    var parens = this.children('.paren');
-    scale(paren, min(1 + .2*(height - 1), 1.2), 1.05*height);
+    var parens = this.jQ.filter('.paren');
+    scale(parens, min(1 + .2*(height - 1), 1.2), 1.05*height);
   };
 });
 
