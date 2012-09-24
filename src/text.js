c4ba68398fc3e9e5cbb4b00c63430ea12efa6727
Han Wei <laughinghan@gmail.com>
2012-09-24 14:01:43 -0700

    first attempt at text.js: TextBlock/TextPiece
    typing $asdf$ works, as does LaTeX rendering of \text{stuff}. Moving
    around and editing doesn't yet, but thanks to all the refactoring,
    should be doable cleanly with .{move,select,delete}{Towards,OutOf}().

---
+++ src/text.js
@@ -0,0 +1,150 @@
+/*************************************************
+ * Abstract classes of text blocks
+ ************************************************/
+
+/**
+ * Blocks of plain text, with one or two TextPiece's as children.
+ * Represents flat strings of typically serif-font Roman characters, as
+ * opposed to hierchical, nested, tree-structured math.
+ * Wraps a single HTMLSpanElement.
+ */
+var TextBlock = P(Node, function(_, _super) {
+  _.ctrlSeq = '\\text';
+
+  _.replaces = function(replacedText) {
+    if (replacedText instanceof Fragment)
+      this.replacedText = replacedText.remove().jQ.text();
+    else if (typeof replacedText === 'string')
+      this.replacedText = replacedText;
+  };
+
+  _.createBefore = function(cursor) {
+    var textBlock = this;
+    _super.createBefore.call(this, cursor);
+
+    if (textBlock[R].respace) textBlock[R].respace();
+    if (textBlock[L].respace) textBlock[L].respace();
+
+    textBlock.bubble('redraw');
+
+    cursor.appendTo(textBlock);
+
+    if (textBlock.replacedText)
+      for (var i = 0; i < textBlock.replacedText.length; i += 1)
+        textBlock.ch[L].write(cursor, textBlock.replacedText.charAt(i));
+  };
+
+  _.parser = function() {
+    var textBlock = this;
+
+    // TODO: correctly parse text mode
+    var string = Parser.string;
+    var regex = Parser.regex;
+    var optWhitespace = Parser.optWhitespace;
+    return optWhitespace
+      .then(string('{')).then(regex(/^[^}]*/)).skip(string('}'))
+      .map(function(text) {
+        var textPc = TextPiece();
+        textPc.initText(text);
+        textPc.adopt(textBlock, 0, 0);
+        return textBlock;
+      })
+    ;
+  };
+
+  _.textContents = function() {
+    return this.foldChildren('', function(text, child) {
+      return text + child.text;
+    });
+  };
+  _.text = function() { return '"' + this.textContents() + '"'; };
+  _.latex = function() { return '\\text{' + this.textContents() + '}'; };
+  _.html = function() {
+    return (
+        '<span class="text" mathquill-command-id='+this.id+'>'
+      +   this.textContents()
+      + '</span>'
+    );
+  };
+
+  _.onKey = function(key, e) {
+    if (key === 'Spacebar' || key === 'Shift-Spacebar') return false;
+  };
+  // backspace and delete at ends of block don't unwrap
+  _.deleteOutOf = function(dir, cursor) {
+    if (this.isEmpty()) cursor.insertAfter(this);
+  };
+  _.write = function(cursor, ch, replacedFragment) {
+    if (replacedFragment) replacedFragment.remove();
+
+    if (ch !== '$') {
+      if (!cursor[L]) TextPiece().createBefore(cursor);
+      cursor[L].appendCh(ch);
+    }
+    else if (this.isEmpty()) {
+      cursor.insertAfter(this);
+      VanillaSymbol('\\$','$').createBefore(cursor);
+    }
+    else if (!cursor[R]) cursor.insertAfter(this);
+    else if (!cursor[L]) cursor.insertBefore(this);
+    else { // split apart
+      var prevBlock = TextBlock();
+      var prevPc = this.ch[L];
+      prevPc.disown();
+      prevPc.adopt(prevBlock, 0, 0);
+
+      cursor.insertBefore(this);
+      _super.createBefore.call(prevBlock, cursor);
+    }
+    return false;
+  };
+  _.blur = MathBlock.prototype.blur;
+  _.focus = MathBlock.prototype.focus;
+  _.isEmpty = MathBlock.prototype.isEmpty;
+});
+
+/**
+ * Piece of plain text, with a TextBlock as a parent and no children.
+ * Wraps a single DOMTextNode.
+ */
+var TextPiece = P(Node, function(_, _super) {
+  _.text = '';
+  _.initText = function(text) { this.text = text; };
+  _.jQize = function() {
+    return this.jQ = document.createTextNode(this.text);
+  };
+  _.appendCh = function(ch) {
+    this.text += ch;
+    this.jQ.appendData(ch);
+  };
+});
+
+CharCmds.$ =
+LatexCmds.text =
+LatexCmds.textnormal =
+LatexCmds.textrm =
+LatexCmds.textup =
+LatexCmds.textmd = TextBlock;
+
+function makeTextBlock(latex, tagName, attrs) {
+  return P(TextBlock, {
+    ctrlSeq: latex,
+    htmlTemplate: '<'+tagName+' '+attrs+'>&0</'+tagName+'>'
+  });
+}
+
+LatexCmds.em = LatexCmds.italic = LatexCmds.italics =
+LatexCmds.emph = LatexCmds.textit = LatexCmds.textsl =
+  makeTextBlock('\\textit', 'i', 'class="text"');
+LatexCmds.strong = LatexCmds.bold = LatexCmds.textbf =
+  makeTextBlock('\\textbf', 'b', 'class="text"');
+LatexCmds.sf = LatexCmds.textsf =
+  makeTextBlock('\\textsf', 'span', 'class="sans-serif text"');
+LatexCmds.tt = LatexCmds.texttt =
+  makeTextBlock('\\texttt', 'span', 'class="monospace text"');
+LatexCmds.textsc =
+  makeTextBlock('\\textsc', 'span', 'style="font-variant:small-caps" class="text"');
+LatexCmds.uppercase =
+  makeTextBlock('\\uppercase', 'span', 'style="text-transform:uppercase" class="text"');
+LatexCmds.lowercase =
+  makeTextBlock('\\lowercase', 'span', 'style="text-transform:lowercase" class="text"');
