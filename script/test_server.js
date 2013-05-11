bd5cefaf145e82e83937867a1cdba0c4215374ba
Han <laughinghan@gmail.com>
2013-05-11 15:30:33 -0700

    if err, 404 or 500 instead of 200-ing a blank response

--- script/test_server.js
+++ script/test_server.js
@@ -11,7 +11,20 @@ http.createServer(function(req, res) {
 
   var filepath = path.normalize(url.parse(req.url).pathname).slice(1);
   fs.readFile(filepath, function(err, data) {
-    res.end(data);
+    if (err) {
+      if (err.code === 'ENOENT' || err.code === 'EISDIR') {
+        res.statusCode = 404;
+        res.end('404 Not Found: /' + filepath + '\n');
+      }
+      else {
+        console.log(err);
+        res.statusCode = 500;
+        res.end('500 Internal Server Error: ' + err.code + '\n');
+      }
+    }
+    else {
+      res.end(data);
+    }
 
     console.log('[%s] %s %s /%s - %s%sms',
       reqTime.toISOString(), res.statusCode, req.method, filepath,
