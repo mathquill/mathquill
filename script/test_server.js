f6e5c6db276f42d112f2cda8493bb913a95bf143
Han <laughinghan@gmail.com>
2013-05-11 14:40:28 -0700

    don't expose entire filesystem to the world
    it's a not a huge deal since the server is only exposed to the local
    network, and you can specify HOST=localhost to restrict to only your
    computer, but still, it's pretty dumb to make it possible for anyone at
    all to read your /etc/passwd with http://localhost:9292//etc/passwd
    
    [path.normalize][] fixes this, collapsing multiple slashes and any
    directory followed by ../, plus removing all leading /../'s (so no
    http://localhost:9292/../../../../../etc/passwd funny business)
    
    [path.normalize]: http://nodejs.org/api/path.html#path_path_normalize_p

--- script/test_server.js
+++ script/test_server.js
@@ -2,11 +2,12 @@ var PORT = +process.env.PORT || 9292;
 var HOST = process.env.HOST || '0.0.0.0';
 
 var http = require('http');
+var path = require('path');
 var url = require('url');
 var fs = require('fs');
 
 http.createServer(function(req, res) {
-  var filepath = url.parse(req.url).pathname.slice(1);
+  var filepath = path.normalize(url.parse(req.url).pathname).slice(1);
   fs.readFile(filepath, function(err, data) { res.end(data); });
 }).listen(PORT, HOST);
 
