8bc76f355494b81484efaebc76238a28fdd2adc5
Han <laughinghan@gmail.com>
2013-05-11 15:16:43 -0700

    log requests, like Connect's static file server
    even better, because it has a timestamp on the request

--- script/test_server.js
+++ script/test_server.js
@@ -7,8 +7,16 @@ var url = require('url');
 var fs = require('fs');
 
 http.createServer(function(req, res) {
+  var reqTime = new Date;
+
   var filepath = path.normalize(url.parse(req.url).pathname).slice(1);
-  fs.readFile(filepath, function(err, data) { res.end(data); });
+  fs.readFile(filepath, function(err, data) {
+    res.end(data);
+
+    console.log('[%s] %s %s /%s - %s%sms',
+      reqTime.toISOString(), res.statusCode, req.method, filepath,
+      (data ? (data.length >> 10) + 'kb, ' : ''), Date.now() - reqTime);
+  });
 }).listen(PORT, HOST);
 
 console.log('listening on '+HOST+':'+PORT);
