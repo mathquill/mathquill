638e149386758ed507b50e18f171df67e1d53fb3
Han <laughinghan@gmail.com>
2014-01-28 15:41:54 -0800

    Move mouseEvents() out into its own mouse.js
    Continue to kill rootelements.js, slowly.

--- src/rootelements.js
+++ src/rootelements.js
@@ -59,68 +59,6 @@ function setupTextarea(editable, container, root, cursor) {
   return textarea;
 }
 
-function mouseEvents(ultimateRootjQ) {
-  //drag-to-select event handling
-  ultimateRootjQ.bind('mousedown.mathquill', function(e) {
-    var rootjQ = $(e.target).closest('.mathquill-root-block');
-    var root = Node.byId[rootjQ.attr(mqBlockId) || ultimateRootjQ.attr(mqBlockId)];
-    var cursor = root.cursor, blink = cursor.blink;
-    var textareaSpan = root.textarea, textarea = textareaSpan.children();
-
-    function mousemove(e) {
-      cursor.seek($(e.target), e.pageX, e.pageY).select();
-      // focus the least-common-ancestor block:
-      if (cursor.selection) cursor.insRightOf(cursor.selection.ends[R]);
-      return false;
-    }
-
-    // docmousemove is attached to the document, so that
-    // selection still works when the mouse leaves the window.
-    function docmousemove(e) {
-      // [Han]: i delete the target because of the way seek works.
-      // it will not move the mouse to the target, but will instead
-      // just seek those X and Y coordinates.  If there is a target,
-      // it will try to move the cursor to document, which will not work.
-      // cursor.seek needs to be refactored.
-      delete e.target;
-
-      return mousemove(e);
-    }
-
-    function mouseup(e) {
-      cursor.endSelection();
-      cursor.blink = blink;
-      if (!cursor.selection) {
-        if (root.editable) {
-          cursor.show();
-        }
-        else {
-          textareaSpan.detach();
-        }
-      }
-
-      // delete the mouse handlers now that we're not dragging anymore
-      rootjQ.unbind('mousemove', mousemove);
-      $(e.target.ownerDocument).unbind('mousemove', docmousemove).unbind('mouseup', mouseup);
-    }
-
-    setTimeout(function() { if (root.blurred) textarea.focus(); });
-      // preventDefault won't prevent focus on mousedown in IE<9
-      // that means immediately after this mousedown, whatever was
-      // mousedown-ed will receive focus
-      // http://bugs.jquery.com/ticket/10345
-
-    cursor.blink = noop;
-    cursor.seek($(e.target), e.pageX, e.pageY).startSelection();
-
-    if (!root.editable && root.blurred) rootjQ.prepend(textareaSpan);
-
-    rootjQ.mousemove(mousemove);
-    $(e.target.ownerDocument).mousemove(docmousemove).mouseup(mouseup);
-    return false;
-  });
-}
-
 function hookUpTextarea(editable, container, root, cursor, textarea, textareaSpan, setTextareaSelection) {
   if (!editable) {
     var textareaManager = manageTextarea(textarea, { container: container });
