8c122cc51512e2f931b1be5ecc5003bbb39872cb
Han <laughinghan@gmail.com>
2014-03-06 22:22:00 -0800

    Deduplicate asserts in typing.test.js #cleanup

--- test/unit/typing.test.js
+++ test/unit/typing.test.js
@@ -8,14 +8,16 @@ suite('typing with auto-replaces', function() {
   });
 
   function prayWellFormedPoint(pt) { prayWellFormed(pt.parent, pt[L], pt[R]); }
+  function assertLatex(latex) {
+    prayWellFormedPoint(mq.controller.cursor);
+    assert.equal(mq.latex(), latex);
+  }
 
   test('LiveFraction', function() {
     mq.typedText('1/2').keystroke('Tab').typedText('+sinx/');
-    prayWellFormedPoint(mq.controller.cursor);
-    assert.equal(mq.latex(), '\\frac{1}{2}+\\frac{\\sin x}{ }');
-
+    assertLatex('\\frac{1}{2}+\\frac{\\sin x}{ }');
     mq.latex('').typedText('1+/2');
-    prayWellFormedPoint(mq.controller.cursor);
-    assert.equal(mq.latex(), '1+\\frac{2}{ }');
+    assertLatex('1+\\frac{2}{ }');
+  });
   });
 });
