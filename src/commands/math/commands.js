67383982c2c00b0bdf51a721306d5c79a4cab336
Michael Desa <mjdesa@gmail.com>
2014-05-05 15:06:30 -0700

    Automatic rewrite of CSS class 'mq-denominator'
    Remaining instances of 'denominator' (outside test/support/):
    README.md:left end of the denominator, right out of the denominator puts the cursor to the
    README.md:the right end of the denominator, etc. Note that right out of the numerator to
    README.md:the left end of the denominator is actually leftwards (and downwards, it's
    README.md:that left into a fraction goes up into the numerator, skipping the denominator;
    README.md:symmetrically, right out of the numerator skips the denominator and puts the
    README.md:actually rightwards (the drawback is the denominator is always skipped, you
    test/unit/publicapi.test.js:    mq.keystroke('Left'); // right edge of denominator
    test/unit/updown.test.js:    assert.equal(denom.latex(), '34', 'denominator is right end child of fraction');
    test/unit/updown.test.js:    assert.equal(cursor.parent, denom, 'cursor down goes into denominator');
    test/unit/updown.test.js:    assert.equal(cursor[L], 0, 'cursor down from numerator inserts at left end of denominator');
    test/unit/updown.test.js:    assert.equal(cursor[R], 0, 'cursor up from denominator inserts at right end of numerator');
    test/unit/updown.test.js:    assert.equal(cursor.parent, denom, 'cursor down goes into denominator');
    test/unit/updown.test.js:    assert.equal(cursor[L], 0, 'cursor down from left of fraction inserts at left end of denominator');
    test/unit/updown.test.js:    assert.equal(cursor.parent, halfDenom, 'cursor down goes into denominator of half');
    test/unit/updown.test.js:    assert.equal(cursor.parent, subFracDenom, 'cursor in subscript fraction denominator');
    test/unit/updown.test.js:    assert.equal(cursor.parent, dxBlock, 'cursor up up from subscript fraction denominator that\s not at right end goes out of subscript');
    test/unit/updown.test.js:    assert.equal(cursor[R], sub, 'cursor up up from subscript fraction denominator that\s not at right end goes before subscript');
    test/unit/updown.test.js:    assert.equal(cursor.parent, subFracDenom, 'cursor in subscript fraction denominator');
    test/unit/updown.test.js:    assert.equal(cursor.parent, dxBlock, 'cursor up up from subscript fraction denominator that is at right end goes out of subscript');
    test/unit/updown.test.js:    assert.equal(cursor[L], sub, 'cursor up up from subscript fraction denominator that is at right end goes after subscript');

--- src/commands/math/commands.js
+++ src/commands/math/commands.js
@@ -347,7 +347,7 @@ LatexCmds.fraction = P(MathCommand, function(_, super_) {
   _.htmlTemplate =
       '<span class="fraction non-leaf">'
     +   '<span class="numerator">&0</span>'
-    +   '<span class="denominator">&1</span>'
+    +   '<span class="mq-denominator">&1</span>'
     +   '<span style="display:inline-block;width:0">&nbsp;</span>'
     + '</span>'
   ;
