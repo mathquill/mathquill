b969050ea02af74090db11e2f217f97325e5c8c2
Michael Desa <mjdesa@gmail.com>
2014-05-05 15:06:32 -0700

    Automatic rewrite of CSS class 'mq-nonSymbola'
    Remaining instances of 'nonSymbola' (outside test/support/):

--- src/commands/math/symbols.js
+++ src/commands/math/symbols.js
@@ -209,7 +209,7 @@ LatexCmds.prime = CharCmds["'"] = bind(VanillaSymbol, "'", '&prime;');
 // does not use Symbola font
 var NonSymbolaSymbol = P(Symbol, function(_, super_) {
   _.init = function(ch, html) {
-    super_.init.call(this, ch, '<span class="nonSymbola">'+(html || ch)+'</span>');
+    super_.init.call(this, ch, '<span class="mq-nonSymbola">'+(html || ch)+'</span>');
   };
 });
 
