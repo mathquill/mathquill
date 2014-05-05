0b74dc9dbf665a2c58562b48448003a2a35dee89
Michael Desa <mjdesa@gmail.com>
2014-05-05 15:06:31 -0700

    Automatic rewrite of CSS class 'mq-focused'
    Remaining instances of 'focused' (outside test/support/):
    src/services/saneKeyboardEvents.util.js:      // when the textarea is not necessarily focused.  We focus it

--- src/services/focusBlur.js
+++ src/services/focusBlur.js
@@ -5,7 +5,7 @@ Controller.open(function(_) {
     ctrlr.textarea.focus(function() {
       ctrlr.blurred = false;
       clearTimeout(blurTimeout);
-      root.jQ.addClass('focused');
+      root.jQ.addClass('mq-focused');
       if (!cursor.parent)
         cursor.insAtRightEnd(root);
       if (cursor.selection) {
@@ -30,7 +30,7 @@ Controller.open(function(_) {
     }
     function blur() { // not directly in the textarea blur handler so as to be
       cursor.hide().parent.blur(); // synchronous with/in the same frame as
-      root.jQ.removeClass('focused'); // clearing/blurring selection
+      root.jQ.removeClass('mq-focused'); // clearing/blurring selection
       $(window).off('blur', windowBlur);
     }
     ctrlr.blurred = true;
