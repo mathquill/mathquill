d837f7bc98a7cb8088c8196e6a388496e1fd1ca5
alec aivazis <alec@aivazis.com>
2015-06-26 15:53:10 -0700

    added initial node module files

--- src/outro.js
+++ src/outro.js
@@ -1,2 +1,11 @@
-
+    if ( typeof exports === 'object' ) {
+        // add the css data to the head tag
+        require('fs').readFile('./mathquill.css', function (err, style_file_contents) {
+          $('head').append('<style>' + style_file_contents + '</style>');
+        });
+        // export the module 
+        module.exports = function(selector, argument){
+          jQuery(selector).mathquill(argument);
+        };
+    }
 }());
