f6271ef504ed4eef42487c2d91feea0c6ceada70
Karol Tarasiuk <karoltarasiuk@gmail.com>
2016-03-17 18:59:52 +1100

    [LRN] Use degree instead of circ for degree symbol

--- src/commands/math/advancedSymbols.js
+++ src/commands/math/advancedSymbols.js
@@ -316,6 +316,7 @@ LatexCmds.cup = LatexCmds.union = bind(BinaryOperator,'\\cup ','&cup;');
 LatexCmds.cap = LatexCmds.intersect = LatexCmds.intersection =
   bind(BinaryOperator,'\\cap ','&cap;');
 
-LatexCmds.deg = LatexCmds.degree = bind(VanillaSymbol,'^\\circ ','&deg;');
+// FIXME \\degree isn't a LaTex command
+LatexCmds.deg = LatexCmds.degree = bind(VanillaSymbol,'\\degree ','&deg;');
 
 LatexCmds.ang = LatexCmds.angle = bind(VanillaSymbol,'\\angle ','&ang;');
