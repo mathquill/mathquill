c4636db8d758b04ecfba4fe681ae99cedbcc619c
Stephen Clower <steve@steve-audio.net>
2016-03-17 08:19:51 -0400

    ARIA alert modification:
    NVDA is the only AT which spits out the word 'alert' any time an ARIA alert fires for something given the aria role of, well, alert. Remove this attribute and depend on remaining attrs to cause AT to speak.
    
    Works with ZoomText Fusion, Window-Eyes, and NVDA. VoiceOver needs to be tested.

--- src/services/aria.js
+++ src/services/aria.js
@@ -16,7 +16,7 @@ var Aria = P(function(_) {
   _.init = function() {
     var el = '.mq-aria-alert';
     // No matter how many Mathquill instances exist, we only need one alert object to say something.
-    if (!jQuery(el).length) jQuery('body').append("<div role='alert' aria-live='assertive' aria-atomic='true' class='mq-aria-alert'></div>"); // make this as noisy as possible in hopes that all modern screen reader/browser combinations will speak when triggered later.
+    if (!jQuery(el).length) jQuery('body').append("<div aria-live='assertive' aria-atomic='true' class='mq-aria-alert'></div>"); // make this as noisy as possible in hopes that all modern screen reader/browser combinations will speak when triggered later.
     this.jQ = jQuery(el);
     this.items = [];
   };
