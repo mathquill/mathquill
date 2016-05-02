7242c19552e803be965e5ea6716eb15d42c85ffb
Stephen Clower <steve@steve-audio.net>
2016-05-02 14:38:29 -0400

    Fix a problem where opening brackets were always spoken even if their closing counterparts were typed.

--- src/services/aria.js
+++ src/services/aria.js
@@ -28,7 +28,17 @@ var Aria = P(function(_) {
         else if (item.ariaLabel) item = item.ariaLabel+' '+item.mathspeak();
         else item = item.mathspeak();
       }
-      else item = item.mathspeak();
+      else {
+        if(item.side) {
+          var ch = '';
+          if(item.side === L) ch = item.textTemplate[0];
+          else if(item.side === R) ch = item.textTemplate[1];
+          this.queueOpenCloseDirOf(item.side);
+          item = BRACKET_NAMES[ch];
+        }
+        else item = item.mathspeak();
+      }
+
     }
     this.items.push(item);
     return this;
@@ -41,7 +51,10 @@ var Aria = P(function(_) {
     prayDirection(dir);
     return this.queue(dir === L ? 'beginning of' : 'end of');
   };
-
+  _.queueOpenCloseDirOf = function(dir) {
+    prayDirection(dir);
+    return this.queue(dir === L ? 'left' : 'right');
+  };
   _.alert = function(t) {
     if (t) this.queue(t);
     if (this.items.length) this.jQ.empty().html(this.items.join(' ').replace(/ +(?= )/g,''));
