e5a74c976044a03e664ed6741e83510b1b09a044
Michael Desa <mjdesa@gmail.com>
2016-05-20 18:38:03 -0700

    Stitch together images to make fullpage screenshot

--- script/screenshots.js
+++ script/screenshots.js
@@ -10,10 +10,14 @@
 // node screenshots.js http://google.com
 
 var wd = require('wd');
+var fs = require('fs');
 var username = process.env['SAUCE_USERNAME'];
 var accessKey = process.env['SAUCE_ACCESS_KEY'];
 var dir = process.env['CIRCLE_ARTIFACTS'] || '/tmp';
 var url = process.argv[2];
+
+dir = dir+'/imgs'
+fs.mkdirSync(dir)
 
 var browserVersions = [
   {
@@ -24,10 +28,10 @@ var browserVersions = [
     'browserName': 'firefox',
     'platform': 'OS X 10.11'
   },
-  {
-    'browserName': 'safari'
-    //'platform': 'OS X 10.10'
-  },
+//  {
+//    'browserName': 'safari',
+//    'platform': ''
+//  },
   {
     'browserName': 'chrome',
     'platform': 'OS X 10.11'
@@ -40,13 +44,18 @@ browserVersions.forEach(function(cfg) {
   // The following is in the style of
   // https://github.com/admc/wd/blob/62f2b0060d36a402de5634477b26a5ed4c051967/examples/async/chrome.js#L25-L40
   browserDriver.init(cfg, function(err, _, cap) {
+    var browser = cfg.browserName.replace(/\s/g, '_')+(!!cap ? '_'+cap.version : '');
+    var platform = (cap ? cap : cfg).platform.replace(/\s/g, '_');
+    var subDir = dir+'/'+browser+'_'+platform;
+    fs.mkdirSync(subDir)
+
     if (err) console.log(err);
 
     browserDriver.get(url, function(err) {
       if (err) console.log(err);
       browserDriver.safeExecute('document.documentElement.scrollHeight', function(err,scrollHeight) {
         if (err) console.log(err);
-        browserDriver.safeExecute('document.documentElement.clientHeight', function(err,windowHeight) {
+        browserDriver.safeExecute('document.documentElement.clientHeight', function(err,viewportHeight) {
           if (err) console.log(err);
 
           var position = 0;
@@ -55,13 +64,13 @@ browserVersions.forEach(function(cfg) {
           // do not. For those browsers we scroll through the page and take
           // incremental screenshots.
           (function loop() {
-            var browser = cfg.browserName.replace(/\s/g, '_')+(!!cap ? '_'+cap.version : '');
-            var platform = (cap ? cap : cfg).platform.replace(/\s/g, '_');
-            var shot = (position/windowHeight) + 1;
+            //var browser = cfg.browserName.replace(/\s/g, '_')+(!!cap ? '_'+cap.version : '');
+            //var platform = (cap ? cap : cfg).platform.replace(/\s/g, '_');
+            var shot = (position/viewportHeight) + 1;
 
             if (cfg.browserName == 'firefox' || cfg.browserName == 'Internet Explorer') {
               // saves file in the file `dir/browser_version_platform.png`
-              var filename = dir+'/'+browser+'_'+platform+'.png';
+              var filename = subDir+'/'+browser+'_'+platform+'.png';
               browserDriver.saveScreenshot(filename, function(err) {
                 if (err) console.log(err);
 
@@ -74,16 +83,40 @@ browserVersions.forEach(function(cfg) {
                 if (err) console.log(JSON.stringify(err));
 
                 // saves file in the file `dir/browser_version_platform_#.png`
-                var filename = dir+'/'+browser+'_'+platform+'_'+shot+'.png';
+                var filename = subDir+'/'+shot+'.png';
                 browserDriver.saveScreenshot(filename, function(err) {
                   if (err) console.log(err);
 
-                  position += windowHeight;
-                  if (position >= scrollHeight) {
-                    browserDriver.quit();
+                  position += viewportHeight;
+                  if (position + viewportHeight > scrollHeight) {
+                    browserDriver.getWindowSize(function(err,size) {
+                      // account for the viewport offset
+                      var extra = size.height - viewportHeight;
+                      browserDriver.setWindowSize(size.width, (scrollHeight-position)+extra, function(err) {
+                        if (err) console.log(err);
+
+                        browserDriver.safeEval('window.scrollTo(0,'+scrollHeight+');', function(err) {
+                          if (err) console.log(JSON.stringify(err));
+
+                          shot++;
+                          var filename = subDir+'/'+shot+'.png';
+                          browserDriver.saveScreenshot(filename, function(err) {
+                            if (err) console.log(err);
+                            browserDriver.quit();
+                          });
+                        });
+
+                      });
+                    });
                   } else {
                     loop();
                   }
+
+                  //if (position >= scrollHeight) {
+                  //  browserDriver.quit();
+                  //} else {
+                  //  loop();
+                  //}
                 });
               });
             }
