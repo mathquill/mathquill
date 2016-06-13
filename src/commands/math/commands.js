a1b081edf793e34fbd669330aa6a563cb0fe54b8
Eoghan McIlwaine <eoghan.mcilwaine@learnosity.com>
2016-06-13 14:45:20 +0100

    Simplify overrightarrow & overleftarrow
    - Use simpler CSS
    - Stop relying on horizontal flip
    - Change arrow characters
    - Align top border with \overline

--- src/commands/math/commands.js
+++ src/commands/math/commands.js
@@ -79,8 +79,19 @@ LatexCmds.mathtt = bind(Style, '\\mathtt', 'span', 'class="mq-monospace mq-font"
 //text-decoration
 LatexCmds.underline = bind(Style, '\\underline', 'span', 'class="mq-non-leaf mq-underline"');
 LatexCmds.overline = LatexCmds.bar = bind(Style, '\\overline', 'span', 'class="mq-non-leaf mq-overline"');
-LatexCmds.overrightarrow = bind(Style, '\\overrightarrow', 'span', 'class="mq-non-leaf mq-overarrow mq-arrow-right"');
-LatexCmds.overleftarrow = bind(Style, '\\overleftarrow', 'span', 'class="mq-non-leaf mq-overarrow mq-arrow-left"');
+
+var OverArrow = P(MathCommand, function(_, super_) {
+  _.init = function(ctrlSeq, attrs) {
+    super_.init.call(
+      this,
+      ctrlSeq,
+      '<span '+attrs+'><span class="mq-arrow-inner">&0</span></span>'
+    );
+  };
+});
+
+LatexCmds.overrightarrow = bind(OverArrow, '\\overrightarrow', 'class="mq-non-leaf mq-overarrow mq-arrow-right"');
+LatexCmds.overleftarrow = bind(OverArrow, '\\overleftarrow', 'class="mq-non-leaf mq-overarrow mq-arrow-left"');
 
 // `\textcolor{color}{math}` will apply a color to the given math content, where
 // `color` is any valid CSS Color Value (see [SitePoint docs][] (recommended),
