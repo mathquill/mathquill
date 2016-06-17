d8654c4cdf41e7298b5546fef53353366d80f870
David Lippman <drlippman@yahoo.com>
2016-06-17 13:12:48 -0700

    Update autoFunctionize to work with changes in 712d1a64aeaf8e132266cc60b464042531e03683

--- src/commands/math/basicSymbols.js
+++ src/commands/math/basicSymbols.js
@@ -81,20 +81,20 @@ var Letter = P(Variable, function(_, super_) {
     if (maxLength > 0) {
       // want longest possible autocommand, so join together longest
       // sequence of letters
-      var str = '', l = this, i = 0;
+      var str = '', l = this, i = 0, donteat = false;
       // FIXME: l.ctrlSeq === l.letter checks if first or last in an operator name
-      while (l instanceof Letter && l.ctrlSeq === l.letter && i < maxLength) {
+      while (l instanceof Letter && i < maxLength) {
+      	if (l.ctrlSeq !== l.letter) { donteat = true;}      
         str = l.letter + str, l = l[L], i += 1;
       }
       // check for an autocommand, going thru substrings longest to shortest
       while (str.length) {
-        if (autoCmds.hasOwnProperty(str)) {
+        if (autoCmds.hasOwnProperty(str) && !donteat) {
           for (var i = 1, l = this; i < str.length; i += 1, l = l[L]);
           Fragment(l, this).remove();
           cursor[L] = l[L];
           return LatexCmds[str](str).createLeftOf(cursor);
         } else if (autoFuncs.hasOwnProperty(str)) {
-          super_.createLeftOf.apply(this, arguments);
           return CharCmds['(']('(').createLeftOf(cursor);
         }
         str = str.slice(1);
