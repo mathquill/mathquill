4e4ad5c7602ff3af876f7c7611e17a673fb178d3
Steve Clower <steve@desmos.com>
2017-08-11 21:41:47 -0400

    Set aria-hidden for cmdId and mathquill-block-id nodes in tree.jqize method

--- src/tree.js
+++ src/tree.js
@@ -179,6 +179,7 @@ var Node = P(function(_) {
       if (el.getAttribute) {
         var cmdId = el.getAttribute('mathquill-command-id');
         if (cmdId) {
+          el.setAttribute('aria-hidden', true);
           el.removeAttribute('mathquill-command-id');
           var cmdNode = TempByIdDict[cmdId]
           cmdNode.jQadd(el);
@@ -187,6 +188,7 @@ var Node = P(function(_) {
 
         var blockId = el.getAttribute('mathquill-block-id');
         if (blockId) {
+          el.setAttribute('aria-hidden', true);
           el.removeAttribute('mathquill-block-id');
           var blockNode = TempByIdDict[blockId]
           blockNode.jQadd(el);
