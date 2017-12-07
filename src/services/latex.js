85c05d86a65b2923c75cb496b82f06b33453a311
Steve Clower <steve@desmos.com>
2017-12-07 12:22:38 -0500

    Add an existence qualifier and rm a comment

--- src/services/latex.js
+++ src/services/latex.js
@@ -285,9 +285,8 @@ Controller.open(function(_, super_) {
     else {
       jQ.empty();
     }
-    var prependedLabel = this.ariaLabel !== 'MathQuill Input' ? this.ariaLabel + ': ' : '';
+    var prependedLabel = this.ariaLabel && this.ariaLabel !== 'MathQuill Input' ? this.ariaLabel + ': ' : '';
     this.container.attr('aria-label', prependedLabel + root.mathspeak().trim());
-    //this.container.attr('aria-label', root.mathspeak().trim());
 
     delete cursor.selection;
     cursor.insAtRightEnd(root);
