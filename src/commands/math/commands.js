4d9525f0e82d694cc51d8d22a560b33b7ef96a92
Emil Laine <emil.laine@reaktor.com>
2018-02-14 17:28:33 +0200

    Add Finnish integral substitution sign

--- src/commands/math/commands.js
+++ src/commands/math/commands.js
@@ -425,6 +425,61 @@ LatexCmds.integral = P(SummationNotation, function(_, super_) {
   _.createLeftOf = MathCommand.p.createLeftOf;
 });
 
+LatexCmds.bigg = // HACK FIXME
+LatexCmds.intsub =
+LatexCmds.integralsubstitution = P(SummationNotation, function(_, super_) {
+  _.init = function() {
+    var htmlTemplate =
+      '<span class="mq-intsub mq-non-leaf">'
+    +   '<big>/</big>'
+    +   '<span class="mq-supsub mq-non-leaf">'
+    +     '<span class="mq-sup"><span class="mq-sup-inner">&1</span></span>'
+    +     '<span class="mq-sub">&0</span>'
+    +     '<span style="display:inline-block;width:0">&#8203</span>'
+    +   '</span>'
+    + '</span>'
+    ;
+    Symbol.prototype.init.call(this, '\\bigg', htmlTemplate);
+  };
+  // FIXME: refactor rather than overriding
+  _.createLeftOf = MathCommand.p.createLeftOf;
+  _.parser = function() {
+    var self = this;
+    var string = Parser.string;
+    var optWhitespace = Parser.optWhitespace;
+    var succeed = Parser.succeed;
+
+    self.blocks = [ MathBlock(), MathBlock() ];
+    for (var i = 0; i < self.blocks.length; i += 1) {
+      self.blocks[i].adopt(self, self.ends[R], 0);
+    }
+
+    return string('/_{\\!\\!\\!\\!\\!')
+      .then(latexMathParser)
+      .then(function(block) {
+        block.children().adopt(self.blocks[0], self.blocks[0].ends[R], 0);
+        return succeed(self);
+      })
+      .then(optWhitespace)
+      .then(string('}'))
+      .then(optWhitespace)
+      .then(string('^'))
+      .then(latexMathParser.block)
+      .then(function(block) {
+        block.children().adopt(self.blocks[1], self.blocks[1].ends[R], 0);
+        return succeed(self);
+      })
+    ;
+  };
+  _.latex = function() {
+    function simplify(latex) {
+      return latex.length === 1 ? latex : '{' + (latex || ' ') + '}';
+    }
+    return '\\bigg/_{\\!\\!\\!\\!\\!' + simplify(this.blocks[0].latex()) +
+      '}^' + simplify(this.blocks[1].latex());
+  };
+});
+
 var Fraction =
 LatexCmds.frac =
 LatexCmds.dfrac =
