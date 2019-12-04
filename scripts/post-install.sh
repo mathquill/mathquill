7b13ce6a03b3acea20c5844994107518b52bad82
kumarranganath <kumar@snapwiz.com>
2019-12-04 11:13:00 +0530

    updated the post-install.sh
    updated the post-install.sh with s3 path.

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -2,4 +2,4 @@ set -e
 cd ~/edulastic-mathquill
 chmod +x  script/*
 make
-aws s3 sync ~/edulastic-mathquill/build s3://edupoc/mathquill
+aws s3 sync ~/edulastic-mathquill/build s3://edupoc/edulasticv2-development/JS/thirdpartylib/mathquill
