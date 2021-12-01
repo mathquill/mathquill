c9425a6ec588e0c1c1abedb7b60d377a03d081d4
Mike Haverstock <mike@desmos.com>
2021-12-01 16:39:17 -0500

    convert Parser to ES6 class

--- src/services/parser.util.js
+++ src/services/parser.util.js
@@ -1,37 +1,39 @@
-var Parser = P(function(_, super_, Parser) {
+
+function parseError(stream, message) {
+  if (stream) {
+    stream = "'"+stream+"'";
+  }
+  else {
+    stream = 'EOF';
+  }
+
+  throw 'Parse Error: '+message+' at '+stream;
+}
+
+class Parser {
   // The Parser object is a wrapper for a parser function.
   // Externally, you use one to parse a string by calling
   //   var result = SomeParser.parse('Me Me Me! Parse Me!');
   // You should never call the constructor, rather you should
   // construct your Parser from the base parsers and the
   // parser combinator methods.
-
-  function parseError(stream, message) {
-    if (stream) {
-      stream = "'"+stream+"'";
-    }
-    else {
-      stream = 'EOF';
-    }
-
-    throw 'Parse Error: '+message+' at '+stream;
+  constructor (body) {
+    this._ = body;
   }
 
-  _.init = function(body) { this._ = body; };
-
-  _.parse = function(stream) {
-    return this.skip(eof)._(''+stream, success, parseError);
+  parse (stream) {
+    return this.skip(Parser.eof)._(''+stream, success, parseError);
 
     function success(stream, result) { return result; }
   };
 
   // -*- primitive combinators -*- //
-  _.or = function(alternative) {
+  or (alternative) {
     pray('or is passed a parser', alternative instanceof Parser);
 
     var self = this;
 
-    return Parser(function(stream, onSuccess, onFailure) {
+    return new Parser(function(stream, onSuccess, onFailure) {
       return self._(stream, onSuccess, failure);
 
       function failure(newStream) {
@@ -40,10 +42,10 @@ var Parser = P(function(_, super_, Parser) {
     });
   };
 
-  _.then = function(next) {
+  then (next) {
     var self = this;
 
-    return Parser(function(stream, onSuccess, onFailure) {
+    return new Parser(function(stream, onSuccess, onFailure) {
       return self._(stream, success, onFailure);
 
       function success(newStream, result) {
@@ -55,10 +57,10 @@ var Parser = P(function(_, super_, Parser) {
   };
 
   // -*- optimized iterative combinators -*- //
-  _.many = function() {
+  many () {
     var self = this;
 
-    return Parser(function(stream, onSuccess, onFailure) {
+    return new Parser(function(stream, onSuccess, onFailure) {
       var xs = [];
       while (self._(stream, success, failure));
       return onSuccess(stream, xs);
@@ -75,11 +77,11 @@ var Parser = P(function(_, super_, Parser) {
     });
   };
 
-  _.times = function(min, max) {
+  times (min, max) {
     if (arguments.length < 2) max = min;
     var self = this;
 
-    return Parser(function(stream, onSuccess, onFailure) {
+    return new Parser(function(stream, onSuccess, onFailure) {
       var xs = [];
       var result = true;
       var failure;
@@ -114,9 +116,9 @@ var Parser = P(function(_, super_, Parser) {
   };
 
   // -*- higher-level combinators -*- //
-  _.result = function(res) { return this.then(succeed(res)); };
-  _.atMost = function(n) { return this.times(0, n); };
-  _.atLeast = function(n) {
+  result (res) { return this.then(Parser.succeed(res)); };
+  atMost (n) { return this.times(0, n); };
+  atLeast (n) {
     var self = this;
     return self.times(n).then(function(start) {
       return self.many().map(function(end) {
@@ -125,20 +127,20 @@ var Parser = P(function(_, super_, Parser) {
     });
   };
 
-  _.map = function(fn) {
-    return this.then(function(result) { return succeed(fn(result)); });
+  map (fn) {
+    return this.then(function(result) { return Parser.succeed(fn(result)); });
   };
 
-  _.skip = function(two) {
+  skip (two) {
     return this.then(function(result) { return two.result(result); });
   };
 
   // -*- primitive parsers -*- //
-  var string = this.string = function(str) {
+  static string (str) {
     var len = str.length;
     var expected = "expected '"+str+"'";
 
-    return Parser(function(stream, onSuccess, onFailure) {
+    return new Parser(function(stream, onSuccess, onFailure) {
       var head = stream.slice(0, len);
 
       if (head === str) {
@@ -150,12 +152,12 @@ var Parser = P(function(_, super_, Parser) {
     });
   };
 
-  var regex = this.regex = function(re) {
+  static regex (re) {
     pray('regexp parser is anchored', re.toString().charAt(1) === '^');
 
     var expected = 'expected '+re;
 
-    return Parser(function(stream, onSuccess, onFailure) {
+    return new Parser(function(stream, onSuccess, onFailure) {
       var match = re.exec(stream);
 
       if (match) {
@@ -168,38 +170,38 @@ var Parser = P(function(_, super_, Parser) {
     });
   };
 
-  var succeed = Parser.succeed = function(result) {
-    return Parser(function(stream, onSuccess) {
+  static succeed (result) {
+    return new Parser(function(stream, onSuccess) {
       return onSuccess(stream, result);
     });
   };
 
-  var fail = Parser.fail = function(msg) {
-    return Parser(function(stream, _, onFailure) {
+  static fail (msg) {
+    return new Parser(function(stream, _, onFailure) {
       return onFailure(stream, msg);
     });
   };
 
-  var letter = Parser.letter = regex(/^[a-z]/i);
-  var letters = Parser.letters = regex(/^[a-z]*/i);
-  var digit = Parser.digit = regex(/^[0-9]/);
-  var digits = Parser.digits = regex(/^[0-9]*/);
-  var whitespace = Parser.whitespace = regex(/^\s+/);
-  var optWhitespace = Parser.optWhitespace = regex(/^\s*/);
+  static letter = Parser.regex(/^[a-z]/i);
+  static letters = Parser.regex(/^[a-z]*/i);
+  static digit = Parser.regex(/^[0-9]/);
+  static digits = Parser.regex(/^[0-9]*/);
+  static whitespace = Parser.regex(/^\s+/);
+  static optWhitespace = Parser.regex(/^\s*/);
 
-  var any = Parser.any = Parser(function(stream, onSuccess, onFailure) {
+  static any = new Parser(function(stream, onSuccess, onFailure) {
     if (!stream) return onFailure(stream, 'expected any character');
 
     return onSuccess(stream.slice(1), stream.charAt(0));
   });
 
-  var all = Parser.all = Parser(function(stream, onSuccess, onFailure) {
+  static all = new Parser(function(stream, onSuccess, onFailure) {
     return onSuccess('', stream);
   });
 
-  var eof = Parser.eof = Parser(function(stream, onSuccess, onFailure) {
+  static eof = new Parser(function(stream, onSuccess, onFailure) {
     if (stream) return onFailure(stream, 'expected EOF');
 
     return onSuccess(stream, stream);
   });
-});
+};
